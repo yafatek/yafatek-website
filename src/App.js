a771ff52665c6c4f889656487ce0ed5e5c0314c0
Feras <feras.alawadi@zarcony.com>
2020-03-19 18:18:15 +0300

    init repo

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import 'bulma/css/bulma.min.css'
+
+
+function App() {
+  return (
+      <div className="App">
+        <section className="section">
+          <div className="container">
+            <h1 className="title">
+              Welcome To YafaTek
+            </h1>
+            <p className="subtitle">
+              The WebSite is under Maintenance
+            </p>
+          </div>
+        </section>
+      </div>
+  );
+}
+
+export default App;
