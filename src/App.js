ea993acbba01c856a3088058aebff0d0f79234bf
Feras <feras.alawadi@zarcony.com>
2020-06-23 21:09:06 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import './App.css';
 import 'bulma/css/bulma.min.css'
+import maintenance from '../src/img/monitor.svg'
 
 
 function App() {
-  return (
-      <div className="App">
-        <section className="section">
-          <div className="container">
-            <h1 className="title">
-              Welcome To YafaTek
-            </h1>
-            <p className="subtitle">
-              The WebSite is under Maintenance
-            </p>
-          </div>
-        </section>
-      </div>
-  );
+    return (
+        <div className="App">
+            <div className="container has-text-centered">
+                <div className="notification is-rounded">
+                    <h1 className="title">
+                        Welcome To YafaTek
+                    </h1>
+                    <p className="subtitle">
+                        The WebSite is under Maintenance
+                    </p>
+                </div>
+                <div className="container">
+                    <figure className="image maintenance-img is-128x128">
+                        <img src={maintenance} alt="Maintenance image"/>
+                    </figure>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
